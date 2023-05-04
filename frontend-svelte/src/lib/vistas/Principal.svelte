7f0a5e169d690543e4de59cc3a024b2f8fff54d0
DanBaDo <bytes4life@proton.me>
2023-05-04 15:59:20 +0200

    First Svelte version

---
+++ frontend-svelte/src/lib/vistas/Principal.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Logout from "../componentes/Logout.svelte";
+
+</script>
+<h2>Página principal</h2>
+<Logout/>
+<p>Esta es la página de inicio de la aplicación.</p>
+<p>Este contenido es de acceso público.</p>
---
+++ frontend-svelte/src/lib/vistas/Privada.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Logout from "../componentes/Logout.svelte";
+import ZonaRestrinxida from "../componentes/ZonaRestrinxida.svelte";
+</script>
+
+<h2>Área privada</h2>
+<Logout/>
+<ZonaRestrinxida>
+    <p>Esta é a túa área privada.</p>
+    <p>É necesario ter unha sesión iniciada para poder acceder.</p>
+</ZonaRestrinxida>
